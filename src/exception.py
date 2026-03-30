c51f51c8c822ce510a425d41d4d9340c699bda62
Kaushik526 <24bai70562@cuchd.in>
2026-03-30 19:32:54 +0530

    exception.py updated

--- src/exception.py
+++ src/exception.py
@@ -9,8 +9,8 @@ def error_message_detail(error,error_detail:sys):
 
 
 class CustomException(Exception):
-    def __int__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
